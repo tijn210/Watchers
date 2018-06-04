7d516a61187cf9d7294b5c31f22659929ad5adee
tijn210 <tijnvv@hotmail.com>
2018-06-04 09:15:34 +0200

    initial commit

--- beheersession/index.php
+++ beheersession/index.php
@@ -17,11 +17,12 @@
 <body>
 <div class="menu">
     <div class="ingriedent-add">
+<!-- ingredient form -->
         <form action="" method="post">
             <table>
                 <tr>
-                    <!--                <td>login</td>-->
                     <td>
+<!-- input ingredient -->
                         <div class="input-group">
                             <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
                             <input id="ingredient" type="text" class="form-control" name="ingredient" placeholder="ingredient">
@@ -29,12 +30,14 @@
                     </td>
                 </tr>
                 <tr>
-                    <!--                <td>Password</td>-->
                     <td>
+<!-- input hoeveelheid -->
                         <div class="input-group margin">
                             <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
                             <input id="hoeveelheid" type="text" class="form-control" name="hoeveelheid" placeholder="hoeveelheid">
+<!-- input eenheid -->
                             <select name="eenheid" class="form-control" id="sel1">
+                                <option value="stuks">stuk(s)</option>
                                 <option value="mg">mg</option>
                                 <option value="g">g</option>
                                 <option value="kg">kg</option>
