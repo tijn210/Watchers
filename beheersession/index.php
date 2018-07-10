ab9c7b4b739cad5712f2b95c45157bc2a52e70b3
tijn210 <tijnvv@hotmail.com>
2018-07-10 15:55:20 +0200

    initial commit

--- beheersession/index.php
+++ beheersession/index.php
@@ -6,12 +6,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Weight Watchers</title>
     <link href="Css/side_menu.css" rel="stylesheet">
-    <link href="../bootstrap/css/bootstrap.css" rel="stylesheet">
-    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../bootstrap/js/bootstrap.min.js" rel="stylesheet">
-    <link href="../bootstrap/js/bootstrap.bundle.min.js" rel="stylesheet">
 </head>
 <body>
 <div class="menu divbg">
